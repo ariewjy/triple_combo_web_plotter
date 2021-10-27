b527b1e06572aa8b24d7a226e239b4e9db97628e
Aditya Arie Wijaya <73975333+ariewjy@users.noreply.github.com>
2021-10-28 03:12:32 +0800

    Add files via upload

--- app.py
+++ app.py
@@ -87,8 +87,8 @@ with data_setting:
   st.sidebar.title('Resistivity Logs')
   res_color = 'purple'
   res_trackname = 'Resistivity'
-  res_left = st.sidebar.number_input('Resistivity Left Scale', min_value=0.00001, max_value=1000000.000)
-  res_right = st.sidebar.number_input('Resistivity Right Scale', min_value=0.00001, max_value=1000000.000)
+  res_left = st.sidebar.number_input('Resistivity Left Scale', min_value=0.00001, max_value=1000000.000, value=0.02)
+  res_right = st.sidebar.number_input('Resistivity Right Scale', min_value=0.00001, max_value=1000000.000, value = 2000)
   res_cutoff = st.sidebar.number_input('Resistivity Cutoff', min_value=0.00001, max_value=1000000.000)
   res_shading = 'lightcoral'
 
