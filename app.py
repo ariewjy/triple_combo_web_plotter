63c2ea9c76c507263222cb878b6c1b15574937f5
Aditya Arie Wijaya <73975333+ariewjy@users.noreply.github.com>
2022-01-04 18:00:14 +0800

    Update app.py

--- app.py
+++ app.py
@@ -18,7 +18,7 @@ sns.set(style='ticks')
 st.set_option('deprecation.showfileUploaderEncoding', False)
 
 st.title('Welcome to Petrophysics Plotter!')
-st.text('This is a web app to plot your LAS 2.0 file data into a triple combo and formation evaluation plots.\n(c) 2021, Aditya Arie Wijaya\n=============================')
+st.text('Plot your LAS 2.0 file into a triple combo and/or formation evaluation plots.\n(c) 2021, Aditya Arie Wijaya\n=============================')
 st.text('Suggestions --> LinkedIn: www.linkedin.com/in/adityaariewijaya89')
 st.text('Tutorial: https://github.com/ariewjy/triple_combo_web_plotter')
 
